e4bacd923085dae2a643b4edabd7651324a2ed6e
tieuma113 <dk.assassins.9x@gmail.com>
2025-09-29 21:36:39 +0700

    update

--- find_minimum_in_rotated_sorted_array.cpp
+++ find_minimum_in_rotated_sorted_array.cpp
@@ -7,25 +7,27 @@ using namespace std;
 
 class Solution {
 public:
-    int findMin(vector<int> &nums) {
-        int result = 0;
-        int l = 0;
-        int r = nums.size()-1;
-        int m = 0;
-        if (nums[l] < nums[r]) return nums[l];
-        while (l < r-1){
-            m = (l + r)/2;
-            if (nums[l] > nums[m]) r = m;
-            else l = m;
-        }
-        nums[l] < nums[r] ? result = nums[l] : result = nums[r];
-        return result;
+  int findMin(vector<int> &nums) {
+    int result = 0;
+    int l = 0;
+    int r = nums.size() - 1;
+    int m = 0;
+    if (nums[l] < nums[r])
+      return nums[l];
+    while (l < r - 1) {
+      m = (l + r) / 2;
+      if (nums[l] > nums[m])
+        r = m;
+      else
+        l = m;
     }
+    nums[l] < nums[r] ? result = nums[l] : result = nums[r];
+    return result;
+  }
 };
 
-
 int main() {
-    vector<int> input = {4,5,6,7};
-    Solution s;
-    s.findMin(input);
+  vector<int> input = {4, 5, 6, 7};
+  Solution s;
+  s.findMin(input);
 }
--- largest_rectangle_in_histogram.cpp
+++ largest_rectangle_in_histogram.cpp
@@ -9,39 +9,39 @@ using namespace std;
 
 class Solution {
 public:
-    int largestRectangleArea(vector<int> &heights) {
-        int result = 0;
-        stack<pair<int, int>> st;
-        int n = heights.size();
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            pair<int, int> temp = make_pair(i, heights[i]);
-            int pos = i;
-            while (!st.empty() && temp.second < st.top().second){
-                int area = (temp.first-st.top().first)*st.top().second;
-                if (max < area){
-                    max = area;
-                }
-                pos = st.top().first;
-                st.pop();
-            }
-            temp.first = pos;
-            st.push(temp);
+  int largestRectangleArea(vector<int> &heights) {
+    int result = 0;
+    stack<pair<int, int>> st;
+    int n = heights.size();
+    int max = 0;
+    for (int i = 0; i < n; i++) {
+      pair<int, int> temp = make_pair(i, heights[i]);
+      int pos = i;
+      while (!st.empty() && temp.second < st.top().second) {
+        int area = (temp.first - st.top().first) * st.top().second;
+        if (max < area) {
+          max = area;
         }
-        while (!st.empty()){
-            int area = (n-st.top().first)*st.top().second;
-            if (max < area){
-                max = area;
-            }
-        }
-        result = max;
-        return result;
+        pos = st.top().first;
+        st.pop();
+      }
+      temp.first = pos;
+      st.push(temp);
     }
+    while (!st.empty()) {
+      int area = (n - st.top().first) * st.top().second;
+      if (max < area) {
+        max = area;
+      }
+    }
+    result = max;
+    return result;
+  }
 };
 
 int main() {
-    vector<int> input = {7,1,7,2,2,4};
-    Solution s;
-    s.largestRectangleArea(input);
-    return 0;
+  vector<int> input = {7, 1, 7, 2, 2, 4};
+  Solution s;
+  s.largestRectangleArea(input);
+  return 0;
 }
